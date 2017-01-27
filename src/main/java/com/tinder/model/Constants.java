bd84d89e45a80507a9125f037eb9ff174b61bd2a
Marc <muilpp@gmail.com>
2017-01-27 16:47:03 +0100

    First commit Spring project

--- src/main/java/com/tinder/model/Constants.java
+++ src/main/java/com/tinder/model/Constants.java
@@ -1,7 +1,7 @@
 package com.tinder.model;
 
 public final class Constants {
-	public final static String FACEBOOK_TOKEN = "EAAGm0PX4ZCpsBAKKIYHPIzagSSXgtbPI8kH683CAWKXZC3jQyhZC1c0gbPq58omEbGuBJIrwlYZCTfrMAbr85T2Jzchy9vH44g8ZAeB5kaFqQ1fjHO7HrLMEJo2ZBysDZCicZCM2UOzl7KiujzZBIJ0VUYysnc8m358OM7azvlXODFMTlTGtD6EdOIdHOw0UWDRGEiXbrePuO7W2wf3y3N8ChHsoWQcl3qqAZD";
+	public final static String FACEBOOK_TOKEN = "";
 	public final static String PAGE_MAIN = "main";
 	public final static String PAGE_RECS = "recs";
 	public final static String PAGE_MATCHES = "matches";
